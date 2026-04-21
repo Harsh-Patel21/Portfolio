cda2413d13137be8861783912facb473b2b180fd
Harsh-Patel21 <harshdiwani21@gmail.com>
2026-04-21 07:29:40 +0530

    h

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,15 @@ import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
+  base: '/Portfolio/',
+
   server: {
     host: true,
     allowedHosts: true,
   },
+
   plugins: [react()],
+
   css: {
     postcss: {
       plugins: [
